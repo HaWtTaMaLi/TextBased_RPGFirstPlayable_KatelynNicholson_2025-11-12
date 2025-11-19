722bdad830d67dd656b4cc703c9523d1ec7b15bf
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-11-19 13:37:05 -0400

    Initial commit

---
+++ TextBased_RPGFirstPlayable_KatelynNicholson_2025-11-12/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace TextBased_RPGFirstPlayable_KatelynNicholson_2025_11_12
+{
+    internal class Program
+    {
+        //Store the map in a text file
+        //Reads it as an array
+        //MAP
+        //walls/boundaries
+        //player cannot move through bounds
+        //Enemy cannot move through bounds
+        //map with only borders is not enough make it visually nice
+
+        //Player
+        //moves WASD/ arrow keys input
+        //attacks enemies by moving into them
+        //can be attacked and killed (by enemy moving into them)
+
+        //Enemy
+        //movies via simple ai
+        //attacks players via moving into them
+        //can be attacked and killed by moving into them
+
+        //refer to Health Systems / and rpg map
+
+        static void Main()
+        {
+            //draw map
+            Map(0);
+
+            //place player and set health
+            Player();
+            PlayerHealth();
+
+            //place enemy and set health
+            Enemy();
+            EnemyHealth();
+
+        }
+
+        static void Map()
+        {
+
+        }
+
+        static void Map(int scale)
+        {
+
+        }
+
+        static void PlayerHealth()
+        {
+
+        }
+
+        static void EnemyHealth()
+        {
+
+        }
+
+        static void Player()
+        {
+
+        }
+
+        static void Enemy()
+        {
+
+        }
+    }
+}
+
